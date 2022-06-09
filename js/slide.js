95adb41721cd2bea27718ed445342208ee090cd8
Leandro Martins <leandro.martins.85@gmail.com>
2022-06-09 06:52:57 -0300

    calculo de distancias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -2,21 +2,33 @@ export default class Slide {
   constructor(slide, wrapper){
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {finalPosition: 0, startX: 0, movement: 0 }
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
-    this.wrapper.addEventListener('mousemove', this.onMove);
-    console.log('start');
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener('mousemove', this.onMove);    
   }
 
-  onMove(){
-    console.log('move');
+  onMove(event){
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(){
     this.wrapper.removeEventListener('mousemove', this.onMove);
-    console.log('end');
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
@@ -30,7 +42,7 @@ export default class Slide {
     this.onMove = this.onMove.bind(this);
   }
 
-  init(){
+  init(){ 
     this.bindEvents();
     this.addSlideEvents();
     return this;
